3435a1060c9f9433657319134edc3f484fc73885
MarkA <m_aguilar_2011@yahoo.com>
2021-01-20 13:33:43 -0600

    sidebar is complete

---
+++ twibber/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Sidebar from "./Sidebar";
+
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Testing Hello</h1>
+
+    {/* Sidebar */}
+    <Sidebar />
+
+    {/* Feed */}
+
+    {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
